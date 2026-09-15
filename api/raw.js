6e870d405f0bd5b0d2563facb29c007835ad99e2
pokeysmoke <130112918+pokeywer@users.noreply.github.com>
2026-09-15 18:45:24 +0800

    Add files via upload

--- api/raw.js
+++ api/raw.js
@@ -2,12 +2,12 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore, doc, getDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    apiKey: "AIzaSyBdAR4ARjHccTlxrmP9tzdYGJxo4MvETXw",
+    authDomain: "voidedx-fe79f.firebaseapp.com",
+    projectId: "voidedx-fe79f",
+    storageBucket: "voidedx-fe79f.firebasestorage.app",
+    messagingSenderId: "784635868195",
+    appId: "1:784635868195:web:6e879214df4238bc2aad96"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
